7bfa8ea3edf30de7a43458272a6cc2c2e3afaf4a
Graham Watson <graham@phantomwatson.com>
2021-02-12 13:07:42 -0500

    Improve appearance of sidebar content

--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -1,89 +1,95 @@
-<h2>
-    United States
-</h2>
-<ul>
-    <li>
-        <?= $this->Html->link(
-            'Housing',
-            [
-                'controller' => 'Data',
-                'action' => 'housing',
-            ]
-        ) ?>
-    </li>
-    <li>
-        <?= $this->Html->link(
-            'Vehicle Sales',
-            [
-                'controller' => 'Data',
-                'action' => 'vehicleSales',
-            ]
-        ) ?>
-    </li>
-    <li>
-        <?= $this->Html->link(
-            'Retail & Food Services',
-            [
-                'controller' => 'Data',
-                'action' => 'retailFoodServices',
-            ]
-        ) ?>
-    </li>
-    <li>
-        <?= $this->Html->link(
-            'Gross Domestic Product',
-            [
-                'controller' => 'Data',
-                'action' => 'gdp',
-            ]
-        ) ?>
-    </li>
-</ul>
+<section>
+    <h2>
+        United States
+    </h2>
+    <ul class="unstyled">
+        <li>
+            <?= $this->Html->link(
+                'Housing',
+                [
+                    'controller' => 'Data',
+                    'action' => 'housing',
+                ]
+            ) ?>
+        </li>
+        <li>
+            <?= $this->Html->link(
+                'Vehicle Sales',
+                [
+                    'controller' => 'Data',
+                    'action' => 'vehicleSales',
+                ]
+            ) ?>
+        </li>
+        <li>
+            <?= $this->Html->link(
+                'Retail & Food Services',
+                [
+                    'controller' => 'Data',
+                    'action' => 'retailFoodServices',
+                ]
+            ) ?>
+        </li>
+        <li>
+            <?= $this->Html->link(
+                'Gross Domestic Product',
+                [
+                    'controller' => 'Data',
+                    'action' => 'gdp',
+                ]
+            ) ?>
+        </li>
+    </ul>
+</section>
 
-<h2>
-    Indiana
-</h2>
-<ul>
-    <li>
-        <?= $this->Html->link(
-            'Unemployment Rate',
-            [
-                'controller' => 'Data',
-                'action' => 'unemployment',
-            ]
-        ) ?>
-    </li>
-    <li>
-        <?= $this->Html->link(
-            'Employment by Sector',
-            [
-                'controller' => 'Data',
-                'action' => 'employmentBySector',
-            ]
-        ) ?>
-    </li>
-    <li>
-        <?= $this->Html->link(
-            'Weekly Earnings',
-            [
-                'controller' => 'Data',
-                'action' => 'earnings',
-            ]
-        ) ?>
-    </li>
-</ul>
+<section>
+    <h2>
+        Indiana
+    </h2>
+    <ul class="unstyled">
+        <li>
+            <?= $this->Html->link(
+                'Unemployment Rate',
+                [
+                    'controller' => 'Data',
+                    'action' => 'unemployment',
+                ]
+            ) ?>
+        </li>
+        <li>
+            <?= $this->Html->link(
+                'Employment by Sector',
+                [
+                    'controller' => 'Data',
+                    'action' => 'employmentBySector',
+                ]
+            ) ?>
+        </li>
+        <li>
+            <?= $this->Html->link(
+                'Weekly Earnings',
+                [
+                    'controller' => 'Data',
+                    'action' => 'earnings',
+                ]
+            ) ?>
+        </li>
+    </ul>
+</section>
 
-<h2>
-    Indiana Counties
-</h2>
-<ul>
-    <li>
-        <?= $this->Html->link(
-            'Unemployment ',
-            [
-                'controller' => 'Data',
-                'action' => 'countyUnemployment',
-            ]
-        ) ?>
-    </li>
-</ul>
+<section>
+    <h2>
+        Indiana Counties
+    </h2>
+    <ul class="unstyled">
+        <li>
+            <?= $this->Html->link(
+                'Unemployment ',
+                [
+                    'controller' => 'Data',
+                    'action' => 'countyUnemployment',
+                ]
+            ) ?>
+        </li>
+    </ul>
+</section>
