60c7f24f04cecfd0f3a72300c978ac409c0efa24
Graham Watson <graham@phantomwatson.com>
2021-03-11 12:24:44 -0500

    Fix how input is expected to be formatted

--- src/Spreadsheet/SpreadsheetSingleDate.php
+++ src/Spreadsheet/SpreadsheetSingleDate.php
@@ -49,22 +49,17 @@ class SpreadsheetSingleDate extends Spreadsheet
         $prepend = Formatter::getPrepend($unit);
         $frequency = Formatter::getFrequency($data);
         foreach ($data['series'] as $name => $series) {
-            $lastObservations = [];
-            foreach (['value', 'change', 'percentChange'] as $type) {
-                $lastObservations[$type] = end($series[$type]);
-            }
-
             $this
                 ->writeRow([
                     $name,
-                    Formatter::formatValue($lastObservations['value']['value'], $prepend),
-                    Formatter::formatValue($lastObservations['change']['value'], $prepend),
-                    Formatter::formatValue($lastObservations['percentChange']['value']) . '%',
+                    Formatter::formatValue($series['value']['value'], $prepend),
+                    Formatter::formatValue($series['change']['value'], $prepend),
+                    Formatter::formatValue($series['percentChange']['value']) . '%',
                 ])
                 ->alignHorizontal('right', 2);
 
             // Write date explicitly as a string so it doesn't get reformatted into a different date format by Excel
-            $date = Formatter::getFormattedDate($lastObservations['value']['date'], $frequency);
+            $date = Formatter::getFormattedDate($series['value']['date'], $frequency);
             $dateCol = 5;
             $cell = $this->getColumnKey($dateCol) . $this->currentRow;
             $this->objPHPExcel
