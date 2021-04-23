268b23907ed4c4a661e0a7449b978a449ebdaf7e
Graham Watson <graham@phantomwatson.com>
2021-04-23 17:39:30 -0400

    Retry failed Slack messages

--- src/Slack/Slack.php
+++ src/Slack/Slack.php
@@ -14,6 +14,16 @@ use Cake\Log\Log;
 class Slack
 {
     /**
+     * @var int RETRY_ATTEMPTS The number of times to retry a failed attempt to send a message
+     */
+    public const RETRY_ATTEMPTS = 2;
+
+    /**
+     * @var float DELAY_BETWEEN_ATTEMPTS The number of seconds to wait after each failed attempt
+     */
+    public const DELAY_BETWEEN_ATTEMPTS = 1;
+
+    /**
      * Sends a message to Slack and logs an error if the attempt fails
      *
      * @param string $text Message to send
@@ -27,9 +37,17 @@ class Slack
         curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, ['Content-Type:application/json']);
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        if (!curl_exec($curlHandle)) {
+
+        // Try to send this message RETRY_ATTEMPTS + 1 times and log any errors
+        for ($attempt = 0; $attempt <= self::RETRY_ATTEMPTS; $attempt++) {
+            if (curl_exec($curlHandle)) {
+                break;
+            }
             Log::error('Error sending message to Slack. Details: ' . curl_error($curlHandle));
+            $microseconds = (int)(self::DELAY_BETWEEN_ATTEMPTS * 1000000);
+            usleep($microseconds);
         }
+
         curl_close($curlHandle);
     }
 }
